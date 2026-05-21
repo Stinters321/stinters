4c6e0bd1fad549159b5c624b17e0094becc49902
atharvasail11 <atharvasail11@gmail.com>
2026-05-21 22:48:45 +0530

    feat: add function to store enquiry form data

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db=getFirestore(app);
+
