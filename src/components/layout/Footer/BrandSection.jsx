9a774aa87a33ec0c5824485c0c53e204f2d53505
atharvasail11 <atharvasail11@gmail.com>
2026-05-22 12:07:36 +0530

    feat: add smooth scroll to footer elements

--- src/components/layout/Footer/BrandSection.jsx
+++ src/components/layout/Footer/BrandSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import FadeUp from '../../FadeUp';
 import Logo from '/logo.png';
+import { handleSmoothScroll } from '../utils/utils';
 
 const FSocial = ({ text }) => {
     return (
@@ -13,7 +14,7 @@ const FSocial = ({ text }) => {
 const BrandSection = () => {
     return (
         <FadeUp delay={0.1} className="flex flex-col text-white/40 gap-2">
-            <img src={Logo} alt="Stintus logo" className='h-14 w-52' />
+            <img src={Logo} alt="Stintus logo" className='h-14 w-52 cursor-pointer' onClick={(e) => handleSmoothScroll(e, '#Hero')}/>
             <p className="text-[9px] text-white/22">MRO · Facility Management · On-Demand Spares</p>
             <p className="text-xs w-[62%] text-white/27">Connecting Indian manufacturing businesses with verified vendors - faster, smarter, with full visibility. Serving Industrial Gujarat.</p>
             <div className="flex items-center gap-2">
--- src/components/layout/Footer/LinkColumn.jsx
+++ src/components/layout/Footer/LinkColumn.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FadeUp from '../../FadeUp';
+import {handleSmoothScroll} from '../utils/utils'
 
 const LinkColumn = ({ title, data, delay }) => {
     return (
@@ -7,7 +8,7 @@ const LinkColumn = ({ title, data, delay }) => {
             <p className="text-[10.4px] font-bold text-white/24" style={{ letterSpacing: '1.04px' }}>{title}</p>
             <div className="flex flex-col text-sm gap-2">
                 {data.map((item, index) => (
-                    <a className="transition-colors hover:text-white" key={index + item.name} href={item.route}>
+                    <a className="transition-colors hover:text-white" onClick={(e)=>handleSmoothScroll(e,item.route)} key={index + item.name} href={item.route}>
                         {item.name}
                     </a>
                 ))}
